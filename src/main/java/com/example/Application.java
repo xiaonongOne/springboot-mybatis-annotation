77b27663c86c8fd4f3611cba57ab0e2444c14f2b
m13522519216@163.com <nishichao123>
2018-12-02 11:54:30 +0800

    SpringBoot集成mybatis注解版本

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,7 +3,7 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 /**
  *@ClassName Application.java
